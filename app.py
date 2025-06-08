6c53b85025a29659686ae49fc15b1f0b4a648d16
santkd16 <santiagocabezas16@gmail.com>
2025-06-07 20:31:55 -0500

    SA

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hola desde Servicio 5'
+    return 'Hola desde Servicio 2'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
